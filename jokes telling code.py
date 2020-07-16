7af75d761bed42bac3552948e37e31800c568188
teamalpha6004 <68186775+teamalpha6004@users.noreply.github.com>
2020-07-16 18:05:18 +0530

    Add files via upload

---
+++ jokes telling code.py
@@ -0,0 +1,9 @@
+
+import webbrowser
+import time
+print("thanks for choosing jokes")
+time.sleep(1.5)
+print("printing joke")
+time.sleep(1.5)
+for x in range(0,2):
+    webbrowser.open("https://www.amarujala.com/humour/love-jokes-jokes-husband-wife-jokes-pappu-jokes-funny-jokes-hindi")
